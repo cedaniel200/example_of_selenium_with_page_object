66e0c97a387a3f5994ee88bf79b40bbe4ea41bdb
cesar.meneses <cesar.meneses@ceiba.com.co>
2018-03-13 17:13:46 -0500

    creation of the maven project

---
+++ example/src/main/java/com/cedaniel200/example/App.java
@@ -0,0 +1,13 @@
+package com.cedaniel200.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
